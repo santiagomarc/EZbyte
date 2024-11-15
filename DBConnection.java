2de6e48a6d4eaf2c334e5cf951659e30454b6729
Marc Santiago <163503280+santiagomarc@users.noreply.github.com>
2024-11-15 12:49:23 +0800

    final DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ezbyte"; // please replace the last word with the name of the connection youve created
+    private static final String USER = "root";
+    private static final String PASSWORD = "root"; // please replace this with your paasword
+    private static Connection connection;
+
+    private DBConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
